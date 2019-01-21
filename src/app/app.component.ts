12b7f93963677b75e68da445e29d5a13ec3e5337
hvekriya <h.vekriya@kainos.com>
2019-01-21 16:46:18 +0000

    Questions are being retrieved from mock data service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,27 +1,66 @@
 import { Component, OnInit } from '@angular/core';
 import { DataService } from './data.service';
+import { NgxSpinnerService } from 'ngx-spinner';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
-  constructor(private quizStore: DataService) {}
+  constructor(
+    private quizStore: DataService,
+    private spinner: NgxSpinnerService
+  ) {}
   quiz = [];
+  questionNo = 0;
+  question = [];
+  inProgress = true;
+  quizOver = false;
+  answerMode = false;
+  correctAns = false;
   ngOnInit() {}
   start() {
+    this.spinner.show();
     // Get the questions from the store
     this.quizStore.getQuiz().then(res => {
       this.quiz = res.questions;
+      this.question = this.quiz[this.questionNo].answers;
+      this.inProgress = false;
+      this.spinner.hide();
+      console.log(this.quiz);
+      console.log(this.question);
     });
   }
   checkAnswer() {
-    console.log('checkAnswer');
+    this.answerMode = true;
+    // this.spinner.show();
+    const id = this.quiz[this.questionNo].id;
+    const index = (<HTMLInputElement>(
+      document.querySelector('input[name="answer"]:checked')
+    )).value;
+    this.quizStore.answerQuestion(id, index).then(res => {
+      console.log(res);
+      this.spinner.hide();
+    });
+    if (!this.correctAns) {
+      this.nextQuestion();
+    }
   }
   nextQuestion() {
-    console.log(this.quiz);
+    if (!this.quizOver && this.questionNo < 7) {
+      this.answerMode = false;
+      this.questionNo++;
+      this.question = this.quiz[this.questionNo].answers;
+      if (this.questionNo > 6) {
+        this.quizOver = true;
+      }
+    }
   }
   reset() {
-    console.log('reset');
+    this.quiz = [];
+    this.questionNo = 0;
+    this.question = [];
+    this.inProgress = true;
+    this.quizOver = false;
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,12 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { NgxSpinnerModule } from 'ngx-spinner';
+
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule, NgxSpinnerModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
