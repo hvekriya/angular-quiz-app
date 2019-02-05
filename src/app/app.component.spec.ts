4fedeb34db888e1cdba41dcf396c8c10fa5d39ed
hvekriya <h.vekriya@kainos.com>
2019-02-05 10:37:02 +0000

    Updated the component level tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,11 +1,15 @@
-import { TestBed, async } from '@angular/core/testing';
+import { TestBed, async, ComponentFixture } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import { NgxSpinnerModule } from 'ngx-spinner';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { DataService } from './data.service';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      imports: [RouterTestingModule],
+      imports: [RouterTestingModule, NgxSpinnerModule, FormsModule],
+      providers: [DataService],
       declarations: [AppComponent]
     }).compileComponents();
   }));
@@ -17,17 +21,19 @@ describe('AppComponent', () => {
   });
 
   it(`should have as title 'TassomaiApp'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('TassomaiApp');
+    const fixture: ComponentFixture<AppComponent> = TestBed.createComponent(
+      AppComponent
+    );
+    const app = fixture.debugElement.nativeElement;
+    expect(app.querySelector('.title').textContent).toEqual('TassomaiApp');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain(
-      'Welcome to the Quiz App!'
+    expect(compiled.querySelector('.greeting').textContent).toContain(
+      'Welcome to the Quiz App'
     );
   });
 });
